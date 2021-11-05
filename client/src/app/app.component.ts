ffff4b79f45da4ff2090af3148cefb2a6bc157bc
Dhrumil <sdhrumil2605@gmail.com>
2021-11-05 10:39:21 -0400

    End of Section 4

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -12,6 +12,10 @@ export class AppComponent implements OnInit{
 
   constructor(private http: HttpClient) {}
   ngOnInit() {
+    this.getUsers();
+  }
+
+  getUsers() {
     this.http.get('https://localhost:5001/api/users').subscribe(response => {
       this.users = response;
     }, error => {
