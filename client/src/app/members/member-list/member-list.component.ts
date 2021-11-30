9f58bfa0112edd93919aa5a8a8c5471eb2030c6b
Dhrumil <sdhrumil2605@gmail.com>
2021-11-29 21:49:58 -0500

    changing head going back to commit

--- client/src/app/members/member-list/member-list.component.ts
+++ client/src/app/members/member-list/member-list.component.ts
@@ -1,13 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
-import { take } from 'rxjs/operators';
 import { Member } from 'src/app/_models/member';
 import { Pagination } from 'src/app/_models/pagination';
 import { User } from 'src/app/_models/user';
 import { UserParams } from 'src/app/_models/userParams';
-import { AccountService } from 'src/app/_services/account.service';
 import { MembersService } from 'src/app/_services/members.service';
-
 @Component({
   selector: 'app-member-list',
   templateUrl: './member-list.component.html',
@@ -25,7 +21,7 @@ export class MemberListComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.loadMembers();
+    this.loadMembers(); 
   }
 
   loadMembers() {
